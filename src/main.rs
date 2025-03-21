06600dbd6dfc12246425adc7ca2ce55ee38db930
Jackson Walters <jacksonwalters@gmail.com>
2025-03-21 16:56:03 -0400

    add AES entropy source
    add entropy source

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use ml_kem::ml_kem::MLKEM;
 use ml_kem::utils::Parameters;
+use aes_ctr_drbg::DrbgCtx;
 mod tests;
 
 fn main() {
@@ -12,4 +13,21 @@ fn main() {
     // Print keys for verification
     println!("Public Key: {:?}", public_key);
     println!("Secret Key: {:?}", secret_key);
+
+    // personalization string must be min. 48 bytes long
+	let p = vec![48, 0];
+
+	// get entropy from somewhere, f.e. /dev/random
+	let entropy: [u8; 48] = [0x04; 48]; // don't use that!
+
+	let mut drbg = DrbgCtx::new();
+	drbg.init(&entropy, p);
+
+	// get 10 bytes
+	let mut out = Vec::new();
+	out.resize(10, 0);
+	drbg.get_random(&mut out);
+
+    println!("{:?}", out);
+
 }
