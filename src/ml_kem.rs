26a41aeeb680fce9758d1902963f7e6e6b8663fe
Jackson Walters <jacksonwalters@gmail.com>
2025-04-01 12:32:51 -0400

    add doctest for `_k_pke_encrypt`

--- src/ml_kem.rs
+++ src/ml_kem.rs
@@ -214,6 +214,24 @@ impl MLKEM {
     /// # Returns
     ///
     /// A vector of bytes representing the encrypted ciphertext.
+    /// 
+    /// # Example
+    /// ```
+    /// use ml_kem::ml_kem::MLKEM;
+    /// use ml_kem::utils::{Parameters,encode_poly,generate_polynomial};
+    /// let params = Parameters::default();
+    /// let mlkem = MLKEM::new(params);
+    /// let d = vec![0x01, 0x02, 0x03, 0x04];
+    /// let (ek_pke, _dk_pke) = mlkem._k_pke_keygen(d);
+    /// let sigma = vec![0u8; 32];
+    /// let eta = 3;
+    /// let n = 0;
+    /// let poly_size = 256;
+    /// let (m_poly, _n) = generate_polynomial(sigma, eta, n, poly_size, None);
+    /// let m = encode_poly(&m_poly,1);
+    /// let r = vec![0x01, 0x02, 0x03, 0x04];
+    /// let k_pke_encrypt_output = mlkem._k_pke_encrypt(ek_pke, m, r);
+    /// ```
     ///
     /// # Panics
     ///
